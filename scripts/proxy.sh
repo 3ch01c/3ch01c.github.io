659e674ad7dac305422583b445b3a7592b2d4a8d
Jack Miner <5547581+3ch01c@users.noreply.github.com>
2020-10-13 23:48:36 -0600

    using command input instead of proxy finder script

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Set proxy environment variables
 
-http_proxy=$(findproxyforurl.sh) 
+#http_proxy=$(findproxyforurl.sh) 
+http_proxy=$1
 if [[ $http_proxy == "" ]]; then
   unset proxy_pac_url
   unset http_proxy
@@ -15,6 +16,7 @@ else
  export HTTP_PROXY=$http_proxy
  export https_proxy=$http_proxy
  export HTTPS_PROXY=$https_proxy
+ # Don't use proxy for the domain of the proxy server
  export no_proxy=127.0.0.1,localhost,$(echo "$http_proxy" | awk -F. '{print "."$2"."$3}' | awk -F: '{print $1}')
  export NO_PROXY=$no_proxy
 fi
