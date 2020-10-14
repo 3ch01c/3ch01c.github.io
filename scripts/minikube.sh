264fb6a4dd07741c238416cc0ace9b05893eb415
Jack Miner <5547581+3ch01c@users.noreply.github.com>
2020-10-13 23:47:45 -0600

    cleaned up minikube script

--- scripts/minikube.sh
+++ scripts/minikube.sh
@@ -2,6 +2,9 @@
 CPUS=4
 MEMORY=4096
 DRIVER="virtualbox"
+DELETE=0
+UPGRADE=0
+VERBOSITY=0
 
 printHelp () {
   echo "A script to create a Minikube VM."
@@ -15,6 +18,10 @@ printHelp () {
 	echo "  -v, --verbose increase output verbosity"
   echo ""
   exit 0
+}
+
+msg_info () {
+  if [[ $VERBOSITY == 1 ]]; then echo "$1"; fi
 }
 
 while [[ $# -gt 0 ]]; do
@@ -36,31 +43,46 @@ countup () {
   printf "\r$2 %02d:%02d:%02d" $((secs/3600)) $(( (secs/60)%60)) $((secs%60))
 }
 
-if [ ! -z ${DELETE+x} ]; then
-  if [ ! -z ${VERBOSITY+x} ]; then echo "* Delete old Minikube instance"; fi
+if [[ $DELETE == 1 ]]; then
+  msg_info "* Delete old Minikube instance"
   minikube delete
 fi
-if [ ! -z ${UPGRADE+x} ]; then
-  if [ ! -z ${VERBOSITY+x} ]; then echo "* Upgrade Minikube"; fi
+if [[ $UPGRADE == 1 ]]; then
+  msg_info "* Upgrade Minikube"
   brew upgrade minikube
 fi
-if [ ! -z ${VERBOSITY+x} ]; then echo "* Start Minikube"; fi
+msg_info "* Start Minikube"
 minikube start --cpus $CPUS --memory $MEMORY --driver $DRIVER #--cni calico --container-runtime containerd
-if [ ! -z ${VERBOSITY+x} ]; then echo "* Set Docker environment variables. To set them in your environment, source this script or run the following command:"; fi
-if [ ! -z ${VERBOSITY+x} ]; then echo '    eval $(minikube docker-env)'; fi
+if [ ! -z ${no_proxy+x} ]; then
+  # Used by the minikube kvm2 driver.
+  if [[ $DRIVER == "kvm2" ]]; then no_proxy=$no_proxy,192.168.39.0/24
+  # Used by the minikube docker driver’s first cluster.
+  elif [[ $DRIVER == "docker" ]]; then no_proxy=$no_proxy,192.168.49.0/24
+  # Used by the minikube VM. Configurable for some hypervisors via --host-only-cidr
+  else no_proxy=$no_proxy,192.168.99.0/24; fi
+  # Used by service cluster IP’s. Configurable via --service-cluster-ip-range
+  no_proxy=$no_proxy,10.96.0.0/12
+  msg_info "* Add Kuberntes service IPs to no_proxy and NO_PROXY environment variables. To set them in your environment, source this script or run the following commands:"
+  msg_info "  export no_proxy=$no_proxy,192.168.99.0/24,192.168.39.0/24,192.168.49.0/24,10.96.0.0/12"
+  msg_info "  export NO_PROXY=$no_proxy"
+  export no_proxy=$no_proxy,$(minikube ip)
+  export NO_PROXY=$no_proxy
+fi
+msg_info "* Set Docker environment variables. To set them in your environment, source this script or run the following command:"
+msg_info '    eval $(minikube docker-env)'
 eval $(minikube docker-env)
 if [ ! -z ${no_proxy+x} ]; then
-  if [ ! -z ${VERBOSITY+x} ]; then echo "* Add Minikube IP to no_proxy and NO_PROXY environment variables. To set them in your environment, source this script or run the following commands:"; fi
-  if [ ! -z ${VERBOSITY+x} ]; then echo "  export no_proxy=$no_proxy,$(minikube ip)"; fi
-  if [ ! -z ${VERBOSITY+x} ]; then echo "  export NO_PROXY=$no_proxy"; fi
+  msg_info "* Add Minikube IP to no_proxy and NO_PROXY environment variables. To set them in your environment, source this script or run the following commands:"
+  msg_info "  export no_proxy=$no_proxy,$(minikube ip)"
+  msg_info "  export NO_PROXY=$no_proxy"
   export no_proxy=$no_proxy,$(minikube ip)
   export NO_PROXY=$no_proxy
 fi
 
 POD=$(kubectl get pod -n kube-system --field-selector=status.phase=Pending -o name)
-if [ ! -z ${VERBOSITY+x} ]; then echo "* You can watch progress with the following commands:"; fi
-if [ ! -z ${VERBOSITY+x} ]; then echo "    watch kubectl get pod -n kube-system  --field-selector=status.phase=Pending"; fi
-if [ ! -z ${VERBOSITY+x} ]; then echo "    kubectl get events -w -n kube-system"; fi
+msg_info "* You can watch progress with the following commands:"
+msg_info "    watch kubectl get pod -n kube-system  --field-selector=status.phase=Pending"
+msg_info "    kubectl get events -w -n kube-system"
 start=$(date +%s)
 while [[ $POD != "" ]]; do
   countup $start "* Wait for Minikube to be ready..."
@@ -68,11 +90,11 @@ while [[ $POD != "" ]]; do
 done
 echo
 
-if [ ! -z ${VERBOSITY+x} ]; then echo "Here's some things you might want to do in your environment:"; fi
-if [ ! -z ${VERBOSITY+x} ]; then echo "* Set Docker environment variables:"; fi
-if [ ! -z ${VERBOSITY+x} ]; then echo '    eval $(minikube docker-env)'; fi
+msg_info "Here's some things you might want to do in your environment:"
+msg_info "* Set Docker environment variables:"
+msg_info '    eval $(minikube docker-env)'
 if [ ! -z ${no_proxy+x} ]; then
-  if [ ! -z ${VERBOSITY+x} ]; then echo "* Add Minikube IP to no_proxy and NO_PROXY environment variables:"; fi
-  if [ ! -z ${VERBOSITY+x} ]; then echo '    export no_proxy=$no_proxy,$(minikube ip)'; fi
-  if [ ! -z ${VERBOSITY+x} ]; then echo '    export NO_PROXY=$no_proxy'; fi
+  msg_info "* Add Minikube IP to no_proxy and NO_PROXY environment variables:"
+  msg_info '    export no_proxy=$no_proxy,$(minikube ip)'
+  msg_info '    export NO_PROXY=$no_proxy'
 fi
