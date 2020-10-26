c9acd770e42459a8b22198dc07c7536ba5b5725d
Jack Miner <5547581+3ch01c@users.noreply.github.com>
2020-10-26 02:15:26 -0600

    photosync release version 1.0

--- scripts/photosync.sh
+++ scripts/photosync.sh
@@ -2,46 +2,40 @@
 
 printHelp () {
   echo "$1"
-  echo "Usage: $0 [-v] INPUT OUTPUT"
-  echo "       INPUT: source directory"
-  echo "       OUTPUT: output directory"
+  echo "Finds photos rooted in SRC and organizes them into subdirectories of DST by date taken (or else last modified date) in Ymd format, e.g., 20201024."
+  echo "Usage: $0 [RSYNC_FLAGS] SRC DST"
+  echo "       RSYNC_FLAGS: Rsync flags. See the rsync man page."
+  echo "       SRC: source root directory"
+  echo "       DST: destination root directory"
   echo ""
   exit 1
 }
 
-IMG_REGEX=".*\.(JPG|RAW|MP4|MOV|360)"
+IMG_REGEX=".*\.(JPG|RAW|MP4|MOV|360|HEIC)"
 
 if [[ $# -lt 2 ]]; then
     printHelp "Not enough arguments"
 fi
-while [[ $# -gt 1 ]]; do
-	key="$1"
-	case "$key" in
-		-h|--help) printHelp ;;
-    -v|--verbose) VERBOSE=1 ;;
-    --delete) DELETE=1 ;;
-    --debug) DEBUG=1 ;;
-		*) SRC_ROOT="$1"; DST_ROOT="$2"
-	esac
-	shift
-done
+RSYNC_FLAGS="${@:1:((${#@}-2))}"
+SRC_ROOT="${@:(-2):1}"
+DST_ROOT="${@:(-1):1}"
 
-if [ ! -z ${DEBUG+x} ]; then echo find "$SRC_ROOT" -regextype posix-extended -iregex "$IMG_REGEX"; fi
-while IFS= read -r IMG
+#echo find "$SRC_ROOT" -regextype posix-extended -iregex "$IMG_REGEX"
+while IFS= read -r SRC
 do
+  if [[ $SRC == "" ]]; then echo "No images found in $SRC_ROOT" && exit 0; fi
   # https://video.stackexchange.com/questions/7145/capture-date-for-video
-  JSON=$(ffprobe -v quiet -print_format json -show_format "$IMG")
+  JSON=$(ffprobe -v quiet -print_format json -show_format "$SRC")
+  # use creation_time tag if available
   CTIME=$(jq -r '.format.tags.creation_time' <<< "$JSON")
-  if [[ $CTIME == "null" ]]; then CTIME=$(stat -c%y "$IMG"); fi
+  # else use file modified time
+  if [[ $CTIME == "null" ]]; then CTIME=$(stat -c%y "$SRC"); fi
   # convert to Ymd
   CTIME=$(date -d"$CTIME" +%Y%m%d)
   CYEAR=$(date -d"$CTIME" +%Y)
   CMONTH=$(date -d"$CTIME" +%m)
   CDAY=$(date -d"$CTIME" +%d)
   DST="$DST_ROOT/$CYEAR/$CMONTH/${CTIME}/"
-  FLAGS="-az"
-  if [ ! -z ${VERBOSE+x} ]; then FLAGS+=" -P"; fi
-  if [ ! -z ${DELETE+x} ]; then FLAGS+=" --remove-source-files"; fi
-  if [ ! -z ${DEBUG+x} ]; then echo rsync $FLAGS "$IMG" "$DST"; fi
-  rsync $FLAGS "$IMG" "$DST"
+  #echo rsync $RSYNC_FLAGS "$SRC" "$DST"
+  rsync $RSYNC_FLAGS "$SRC" "$DST"
 done <<< $(find "$SRC_ROOT" -regextype posix-extended -iregex "$IMG_REGEX")
