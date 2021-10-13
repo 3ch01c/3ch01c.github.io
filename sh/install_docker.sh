63c2a9a4e733073e9f09bce68148747d2bd2b1e8
Jack Miner <5547581+3ch01c@users.noreply.github.com>
2021-10-13 14:46:02 -0600

    updated docker-compose version on doicker install script

--- sh/install_docker.sh
+++ sh/install_docker.sh
@@ -2,20 +2,22 @@
 # Install Docker, docker-compose, and add the current user to the docker group.
 
 DOCKER_VERSION="get" # or "test" for latest
-DOCKER_COMPOSE_VERSION="1.27.4"
+DOCKER_COMPOSE_VERSION="v2.0.1"
 
 # Install docker
 curl -fsSL https://$DOCKER_VERSION.docker.com | sh -
 
-# Install docker-compose
-sudo curl -fsSL "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
 # Add user to docker group
-sudo usermod -aG docker $(whoami)
+sudo usermod -aG docker $USER
+
 # Make docker the user's current group (so they don't have to logout and back in)
 newgrp docker
 
 # Test some docker commands.
 docker ps
 docker run hello-world
+
+# Install docker-compose
+sudo curl -fsSL "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
